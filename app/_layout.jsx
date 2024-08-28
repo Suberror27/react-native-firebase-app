cbc7c7cf06eb47489996060272d35fd4e84efe34
SuberrorDesktop <abrahamct27@gmail.com>
2024-08-27 23:50:12 -0500

    ready to start

---
+++ app/_layout.jsx
@@ -0,0 +1,23 @@
+//This page will be present on all pages (Good for Navbar)
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Slot, Stack } from 'expo-router'
+
+export default function RootLayout() {
+  return (
+    <>
+    <Stack>
+      <Stack.Screen name="index" options={{headerShown: false}} />
+    </Stack>
+    </>
+)
+}
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  }
+})
---
+++ app/index.jsx
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-black">
+      <Text className="text-3xl text-white">Home Page</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
